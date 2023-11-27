9fa062fce82cd000089bbcee81bf0e5b42df33c6
nguoingoaihanhtinh <147408824+nguoingoaihanhtinh@users.noreply.github.com>
2023-11-27 14:55:07 +0700

    Add files via upload

---
+++ Chuong7_22520688_PhanTuanAnhKhoa/Chuong7_22520688_PhanTuanAnhKhoa/Font_List/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Font_List
+{
+    public partial class Form1 : Form
+    {
+        private Label sampleTextLabel;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            sampleTextLabel = new Label();
+            sampleTextLabel.AutoSize = true;
+            Controls.Add(sampleTextLabel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] fontNames = FontFamily.Families.Select(f => f.Name).ToArray();
+
+            // Tạo và cấu hình ListView để hiển thị danh sách font
+            ListView fontListView = new ListView();
+            fontListView.Dock = DockStyle.Left;
+            fontListView.Width = 200;
+            fontListView.View = View.Details;
+            fontListView.Columns.Add("Font", 200);
+
+            // Thêm mỗi font vào ListView
+            foreach (var fontName in fontNames)
+            {
+                ListViewItem item = new ListViewItem(fontName);
+                fontListView.Items.Add(item);
+            }
+
+            // Thêm sự kiện để vẽ một mẫu văn bản cho mỗi font khi chọn font
+            fontListView.SelectedIndexChanged += FontListView_SelectedIndexChanged;
+
+            // Thêm ListView vào Form
+            Controls.Add(fontListView);
+        }
+
+
+
+
+        private void FontListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sender is ListView fontListView && fontListView.SelectedItems.Count > 0)
+            {
+                // Lấy font được chọn
+                string selectedFontName = fontListView.SelectedItems[0].Text;
+                Font selectedFont = new Font(selectedFontName, 12); // 12 là kích thước font
+
+                // Hiển thị mẫu văn bản bằng font được chọn
+                sampleTextLabel.Text = "Hello, Font!";
+                sampleTextLabel.Font = selectedFont;
+            }
+        }
+    }
+
+}
---
+++ Chuong7_22520688_PhanTuanAnhKhoa/Chuong7_22520688_PhanTuanAnhKhoa/String_Name/Form1.cs
@@ -0,0 +1,48 @@
+﻿namespace String_Name
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Lấy kích thước của form
+            int formWidth = this.ClientSize.Width;
+            int formHeight = this.ClientSize.Height;
+
+            // Xác định cạnh của hình vuông
+            int squareSize = Math.Min(formWidth, formHeight);
+
+            // Tạo một mảng chứa 4 tên
+            string[] names = { "Phan", "Tuấn", "Anh", "Khoa" };
+
+            // Tạo một font cho văn bản
+            Font font = new Font("Arial", 12);
+
+            // Vẽ các chuỗi lần lượt theo chiều kim đồng hồ
+            for (int i = 0; i < names.Length; i++)
+            {
+                // Tính toán vị trí và kích thước cho văn bản
+                float angle = (float)(i * Math.PI / 2); // Góc trong radian
+                float x = formWidth / 2 + (float)(Math.Cos(angle) * squareSize / 3);
+                float y = formHeight / 2 + (float)(Math.Sin(angle) * squareSize / 3);
+
+                // Lấy kích thước của chuỗi
+                SizeF textSize = e.Graphics.MeasureString(names[i], font);
+
+                // Vẽ chuỗi vào vị trí đã tính toán
+                e.Graphics.DrawString(names[i], font, Brushes.Black, x - textSize.Width / 2, y - textSize.Height / 2);
+            }
+        }
+    }
+}
